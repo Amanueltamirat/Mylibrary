c6b655f1b9fbc34142c5c1beabedc30927f47785
AmanuelTamirat <amanuel.tamirat22@gmail.com>
2023-10-06 23:44:57 +0300

    initial author routes

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ if (process.env.NODE_ENV !== "production") {
 const express = require("express");
 const app = express();
 const indexRouters = require("./controllers/index");
+const authorsRouters = require("./controllers/authors");
 const expressLayouts = require("express-ejs-layouts");
+const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 const db = mongoose.connection;
@@ -15,8 +17,10 @@ app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
 app.use(express.static("public"));
 
 app.use("/", indexRouters);
+app.use("/authors", authorsRouters);
 
 app.listen(process.env.PORT || 3000);
